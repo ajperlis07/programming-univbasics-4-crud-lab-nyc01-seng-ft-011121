d51f226b640374792e5562ef63b160d956bc52cf
Andrew  Perlis <ajperlis07@gmail.com>
2020-12-31 17:06:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,19 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  systems = [ "xbox" , "playstation" , "switch" , "pc" ]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  add_element = [ "array" , "element"]
+  add_element << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  add_start = [ "array" , "element"]
+  add_start.unshift("wow") 
 end
 
 def remove_element_from_end_of_array(array)
