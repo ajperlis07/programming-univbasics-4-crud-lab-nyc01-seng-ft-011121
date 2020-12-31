5ac247262a51803a6dad4552423f26d03a77ab34
Andrew  Perlis <ajperlis07@gmail.com>
2020-12-31 17:10:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  remove_element = ["array"]
+  remove_element = ["array" , "arrays!"]
   remove = remove_element.pop
 end
 
